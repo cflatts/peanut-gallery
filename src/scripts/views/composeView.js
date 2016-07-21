fd75fe8c86cde362836758eeeb620e34915c4d65
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-21 10:47:20 -0500

    created new dish object and saved to db

--- src/scripts/views/composeView.js
+++ src/scripts/views/composeView.js
@@ -21,7 +21,15 @@ const DishPostingForm = React.createClass({
     _handleSubmit: function(evt) {
         evt.preventDefault() //keeps the page from refreshing everytime you interact with the form (hold over from early internet)
 
-        ACTIONS.saveDish(dishObj)
+        ACTIONS.saveDish({ //create the dish object right here on save (get the keys from the schema we set up)
+            title: evt.currentTarget.title.value,
+            description: evt.currentTarget.description.value,
+            location: evt.currentTarget.description.value,
+            rating: evt.currentTarget.rating.value,
+            authorId: User.getCurrentUser()._id,
+            authorEmail: User.getCurrentUser().email,
+            imageUrl: ''
+            })
     },
 
 	render: function() {
@@ -30,6 +38,8 @@ const DishPostingForm = React.createClass({
                 <form onSubmit = {this._handleSubmit}>
                     <input type = 'text' name = 'title' placeholder = 'Title' />
                     <textarea name = 'description' placeholder = 'Tell me about your meal!'></textarea>
+                    <input type = 'text' name = 'location' placeholder = 'Tell me where you ate!'/>
+                    <input type = 'text' name = 'rating' />
                     <button type = 'submit'>Submit</button>
 
                 </form>
