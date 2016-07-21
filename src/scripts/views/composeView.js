f5ba646ad65dcff362aeb4f1299ec0d93d81f92a
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-21 10:37:05 -0500

    assigned handler to onsumbit of compose

--- src/scripts/views/composeView.js
+++ src/scripts/views/composeView.js
@@ -6,19 +6,19 @@ const ComposeView = React.createClass({
 	 	return (
 	 		<div className = 'composeView' >
 	 			<Header />
-	 			<h3>post a dish!</h3>
+	 			<h3>Post a dish!</h3>
 	 			<DishPostingForm />
 	 		</div>
 	 	)
  	}
 })
 
-//STEP 7 (CREATE FORM TO COMPOSE NEWP OST)
+//STEP 7 (CREATE FORM TO COMPOSE NEW POST)
 const DishPostingForm = React.createClass({
 	render: function() {
 		return (
 			<div className = 'dishPostingForm'>
-                <form>
+                <form onSubmit = {this._handleSubmit}>
                     <input type = 'text' name = 'title' placeholder = 'Title' />
                     <textarea name = 'description' placeholder = 'Tell me about your meal!'></textarea>
                     <button type = 'submit'>Submit</button>
