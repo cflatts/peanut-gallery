a95a4aaa30dae9ffd0f2c331c01502e6d0dd0516
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-21 11:11:12 -0500

    created restricted routes for non-users

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,6 +6,7 @@ import LoginView from './views/loginView'
 import Dashboard from './views/dashboard'
 import DishesView from './views/dishesView'
 import ComposeView from './views/composeView'
+import {User} from './models/models'
 
 //STEP 5 (build your client side api routes)
 const app = function() {
@@ -13,7 +14,7 @@ const app = function() {
     var AppRouter = Backbone.Router.extend ({
         routes: {
             'home': 'goHome',
-            'dish/postDishes': 'handleDishPost', // VVV these routes were determined by the routes we used in the Header
+            'dish/postDishes': 'handleDishPost', // VVV these routes were determined by the navbar we created in the Header
             'dish/myDishes': 'handleMyPosts',
             'login': 'handleLogin',
             '*catchall': 'redirectHome'
@@ -41,6 +42,13 @@ const app = function() {
 
         initialize: function() { //good way to add logic to check if a user is logged in to protect certain routes
             Backbone.history.start()
+        //STEP 10 (WE ARE ADDING THAT LOGIC I TALKED ABOUT IN THE PREVIOUS COMMENT)
+            this.on('route', function(handlerName) {//when there is a route Backbone will pass in one of the route handler methods we have in the router
+                if(!User.getCurrentUser()) {
+                    location.hash = 'login'
+                } //this makes sure that if the person using the app isn't logged in, all they can access is the login page
+            })
+
         }
     })
     new AppRouter()
